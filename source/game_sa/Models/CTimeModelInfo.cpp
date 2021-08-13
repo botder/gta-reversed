7e90c1bcbb03ad6e91cf8a8f516996546ef0ea89
Marek Kulik <me@botder.com>
2021-08-13 19:36:03 +0200

    Reverse CTimeModelInfo::FindOtherTimeModel

--- source/game_sa/Models/CTimeModelInfo.cpp
+++ source/game_sa/Models/CTimeModelInfo.cpp
@@ -1,8 +1,10 @@
 #include "StdInc.h"
+#include "CModelInfo.h"
 
 void CTimeModelInfo::InjectHooks() {
     ReversibleHooks::Install("CTimeModelInfo", "GetModelType", 0x4C5660, &CTimeModelInfo::GetModelType_Reversed);
     ReversibleHooks::Install("CTimeModelInfo", "GetTimeInfo", 0x4C5670, &CTimeModelInfo::GetTimeInfo_Reversed);
+    ReversibleHooks::Install("CTimeModelInfo", "FindOtherTimeModel", 0x4C47E0, &CTimeModelInfo::FindOtherTimeModel);
 }
 
 // 0x4C5640
@@ -27,7 +29,36 @@ tTimeInfo* CTimeModelInfo::GetTimeInfo() {
 
 // 0x4C47E0
 tTimeInfo* CTimeModelInfo::FindOtherTimeModel(const char* modelName) {
-    return plugin::CallMethodAndReturn<tTimeInfo*, 0x4C47E0, CTimeModelInfo*, const char*>(this, modelName);
+    char timeSwitchModelName[24];
+    strcpy(timeSwitchModelName, modelName);
+
+    char* nightSuffix = strstr(timeSwitchModelName, "_nt");
+
+    if (nightSuffix)
+    {
+        strncpy(nightSuffix, "_dy", 4);
+    }
+    else
+    {
+        char* daySuffix = strstr(timeSwitchModelName, "_dy");
+
+        if (!daySuffix)
+            return nullptr;
+
+        strncpy(daySuffix, "_nt", 4);
+    }
+
+    int index = 0;
+    CBaseModelInfo* modelInfo = CModelInfo::GetModelInfoFromHashKey(CKeyGen::GetUppercaseKey(timeSwitchModelName), &index);
+    tTimeInfo* timeInfo = modelInfo ? modelInfo->GetTimeInfo() : nullptr;
+
+    if (timeInfo)
+    {
+        m_timeInfo.m_nOtherTimeModel = index;
+        return timeInfo;
+    }
+
+    return nullptr;
 }
 
 ModelInfoType CTimeModelInfo::GetModelType_Reversed() {
